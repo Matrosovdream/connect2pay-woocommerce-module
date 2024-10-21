2d9ee72843407264347f713789a866df95724bc5
dotfiler <dotfiler@gmail.com>
2024-10-21 15:20:48 +0200

    Deep optimization, HPOS support

---
+++ views/iframe-checkout.php
@@ -0,0 +1,11 @@
+<iframe 
+    id="payxpert_for_woocommerce_iframe" 
+    src="<?php echo $payxpert_customer_url; ?>" 
+    width="100%" 
+    height="700"
+    scrolling="no" 
+    frameborder="0" 
+    border="0" 
+    allowtransparency="true"
+    >
+</iframe>
